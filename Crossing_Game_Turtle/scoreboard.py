545869e700f820ed9ad3685c6d7fc3c245aeb770
nphucdev0111 <nphuc0111dev@gmail.com>
2023-11-18 13:16:58 +0700

    ADD CROSSING GAME

---
+++ Crossing_Game_Turtle/scoreboard.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.goto(-280, 250)
+        self.level = 1
+        self.update()
+
+    def update(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align= "center", font=FONT)
+
+
