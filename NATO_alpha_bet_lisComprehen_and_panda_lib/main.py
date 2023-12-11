d6b35bf4217d980d2315cac985e193fe3209f3b5
nphucdev0111 <nphuc0111dev@gmail.com>
2023-12-11 11:51:14 +0700

    Add Handling error

--- NATO_alpha_bet_lisComprehen_and_panda_lib/main.py
+++ NATO_alpha_bet_lisComprehen_and_panda_lib/main.py
@@ -4,7 +4,12 @@ import pandas
 alphabet_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter: row.code for (index, row) in alphabet_frame.iterrows()}
 
-
-user_input = input("Enter a word:").strip().upper()
-phonetic_list = [alphabet_dict[char] for char in user_input]
-print(phonetic_list)
+while True:
+    user_input = input("Enter a word: ").strip().upper()
+    try:
+        phonetic_list = [alphabet_dict[char] for char in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(phonetic_list)
+        break
